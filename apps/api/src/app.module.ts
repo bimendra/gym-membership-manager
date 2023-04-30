04f2e29748dfd61cdf3401da6c1a9c0c0915a7e4
Bimendra Bandara <bimendra.bandara@velaris.io>
2023-05-01 00:08:28 +0800

    Import prisma module into app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,12 +3,14 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../..', 'client', 'dist'),
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
