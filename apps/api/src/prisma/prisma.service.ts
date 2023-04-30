c9ce9af0feb1c13a0df60e0bb9ff9575d36cab2f
Bimendra Bandara <bimendra.bandara@velaris.io>
2023-05-01 00:05:09 +0800

    Create prisma module

---
+++ apps/api/src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'mongodb+srv://bimendra:E2xWzvj9aN77mRfz@default.h7aoyps.mongodb.net/developer-code-test?retryWrites=true&w=majority',
+        },
+      },
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
