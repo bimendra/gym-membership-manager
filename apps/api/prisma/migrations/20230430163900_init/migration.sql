844aca15165ca94807744d81384d1f4fa5e364fc
Bimendra Bandara <bimendra.bandara@velaris.io>
2023-05-01 00:39:47 +0800

    Migrate db

---
+++ apps/api/prisma/migrations/20230430163900_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "avatar" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
